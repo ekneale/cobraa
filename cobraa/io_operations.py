a8baeac023e61867a1246ee5fc97e216ecefdec0
ekneale <eskneale1@sheffield.ac.uk>
2021-05-20 12:16:44 +0100

    Fixes typo in jobSubmissionCommands function

--- cobraa/io_operations.py
+++ cobraa/io_operations.py
@@ -374,7 +374,7 @@ for i in `seq {nruns}`; do source {script}; done
 
     """ 
 
-    elif arguments['--cluster']=='--edinburgh':
+    elif arguments['--cluster']=='edinburgh':
         jobheader = f"""#!/bin/sh
 
 qsub -t 40 -V -q ppe.7.day -N job_{_element} -j y -cwd {script}
