17946db567122b46f1caa55b266a3f475fd161cd
Steve Wilson <stw4198@aol.com>
2021-06-30 22:22:37 +0100

    heysham 2 and torness added in for MC

--- cobraa/load.py
+++ cobraa/load.py
@@ -106,6 +106,8 @@ docstring = """
     -G=<Goodness>                Bonsai direction goodness parameter [Default: 0.1]
     --se=<_se>                   Default signal efficiency [Default: 1.00]
     --Heysham                    Uses Heysham reactor spectrum in place of Hartlepool
+    --Heysham2                   Uses Heysham 2 reactor spectrum in place of Hartlepool
+    --Torness                    Uses Torness reactor spectrum in place of Hartlepool
     """
 
 try:
@@ -117,7 +119,10 @@ except ImportError:
 
 if (arguments['--Heysham']):
         print("Using Heysham spectrum (all 4 cores) and assuming Hartlepool is off")
-
+if (arguments['--Heysham2']):
+        print("Using Heysham 2 spectrum (2 cores) and assuming Hartlepool, Heysham 1 are off")
+if (arguments['--Torness']):
+        print("Using Torness spectrum (2 cores) and assuming Hartlepool, Heysham are off")
 
 def loadSimulationParameters():
     #Chain and subsequent isotopes
@@ -159,7 +164,7 @@ def loadSimulationParameters():
         d['RADIOGENIC'] = {'ROCK_2':['rock_neutrons']}
 
 
-        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background']}
+        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background','heysham2_signal','heysham2_background','torness_signal','torness_background']}
 
         d['singles'] = {'ALL':['singles']}
         d['A_Z'] = {'LIQUID':['li 9','n 17']}
@@ -218,7 +223,7 @@ def loadSimulationParameters():
         d['ibd_p_hs'] = {'LIQUID':['IBDPositronHeyshamSig']}
         d['ibd_p_hb'] = {'LIQUID':['IBDPositronHeyshamBkg']}
         d['ibd_n'] = {'LIQUID':['IBDNeutron']}
-        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background']}
+        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background','heysham2_signal','heysham2_background','torness_signal','torness_background']}
 
         d['singles'] = {'ALL':['singles']}
         d['A_Z'] = {'LIQUID':['li 9','n 17']}
@@ -287,7 +292,7 @@ def loadSimulationParameters():
         d['ibd_p_hs'] = {'LIQUID':['IBDPositronHeyshamSig']}
         d['ibd_p_hb'] = {'LIQUID':['IBDPositronHeyshamBkg']}
         d['ibd_n'] = {'LIQUID':['IBDNeutron']}
-        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background']}
+        d['pn_ibd'] = {'LIQUID':['boulby_geo','big_hartlepool','small_hartlepool','boulby_world','heysham_signal','heysham_background','heysham2_signal','heysham2_background','torness_signal','torness_background']}
 
         d['singles'] = {'ALL':['singles']}
         d['A_Z'] = {'LIQUID':['li 9','n 17']}
@@ -336,10 +341,6 @@ def loadSimulationParameters():
         elif int(arguments['--cylinderPct'])==15:
             iPMTs = 2552./3248.
             pmtVolCorr = 3206.17/3203.22
-    elif int(arguments['--cylinderSize'])==22 and int(arguments['--rPMT'])==9000:
-        if int(arguments['--cylinderPct'])==15:
-            iPMTs = 4600./4600.
-            pmtVolCorr = 1
     elif int(arguments['--cylinderPct'])==16 and int(arguments['--rPMT'])==6700:
         if int(arguments['--cylinderPct'])==10:
             iPMTs = 1232./2230.
@@ -483,28 +484,32 @@ def loadSimulationParameters():
     elif int(arguments['--cylinderSize'])==22 and int(arguments['--rPMT'])==9000:
         print('Using rates for 22 m cylinder with 9 m inner PMT radius and 15% PC (Passive buffer)')
         jobRate = {\
-'IBDPositron_LIQUID_ibd_p': [ 1.262e-04*pmtVolCorr , 1], \
-'IBDPositronHeyshamSig_LIQUID_ibd_p_hs': [1.195e-05*pmtVolCorr , 1], \
-'IBDPositronHeyshamBkg_LIQUID_ibd_p_hb': [3.290e-05*pmtVolCorr, 1], \
-'IBDNeutron_LIQUID_ibd_n': [ 1.262e-04*pmtVolCorr, 1], \
-'big_hartlepool_LIQUID_pn_ibd': [1.262e-04*pmtVolCorr , 1],\
-'small_hartlepool_LIQUID_pn_ibd': [8.753e-05*pmtVolCorr , 1],\
-'boulby_geo_LIQUID_pn_ibd': [9.288e-07*pmtVolCorr , 1],\
-'boulby_world_LIQUID_pn_ibd': [5.803e-06*pmtVolCorr , 1],\
-'heysham_signal_LIQUID_pn_ibd': [1.195e-05*pmtVolCorr , 1],\
-'heysham_background_LIQUID_pn_ibd': [3.290e-05*pmtVolCorr , 1],\
-'40K_LIQUID_40K_NA': [34.3*pmtVolCorr , 1], \
-'40K_PMT_40K_NA': [1.67E+04 *iPMTs * kip, 1], \
+'IBDPositron_LIQUID_ibd_p': [ 1.050e-04 , 1], \
+'IBDPositronHeyshamSig_LIQUID_ibd_p_hs': [1.22e-05 , 1], \
+'IBDPositronHeyshamBkg_LIQUID_ibd_p_hb': [3.040e-05, 1], \
+'IBDNeutron_LIQUID_ibd_n': [ 1.050e-04, 1], \
+'big_hartlepool_LIQUID_pn_ibd': [1.050e-05, 1],\
+'small_hartlepool_LIQUID_pn_ibd': [7.861e-05, 1],\
+'boulby_geo_LIQUID_pn_ibd': [6.364e-06 , 1],\
+'boulby_world_LIQUID_pn_ibd': [3.624e-05 , 1],\
+'heysham_signal_LIQUID_pn_ibd': [1.22e-05 , 1],\
+'heysham_background_LIQUID_pn_ibd': [3.040e-05 , 1],\
+'heysham2_signal_LIQUID_pn_ibd': [6.894e-06 , 1],\
+'heysham2_background_LIQUID_pn_ibd': [3.040e-05 , 1],\
+'torness_signal_LIQUID_pn_ibd': [4.419e-06 , 1],\
+'torness_background_LIQUID_pn_ibd': [1.962e-06 , 1],\
+'40K_LIQUID_40K_NA': [34.3 , 1], \
+'40K_PMT_40K_NA': [1.67E+04 * kip, 1], \
 '40K_VETO_40K_NA': [0 * kip, 1], \
 '40K_IBEAM_40K_NA': [3.04E+04, 500], \
 '40K_PSUP_40K_NA': [4.19E+03 , 1], \
 '40K_TANK_40K_NA': [4.81E+04, 50], \
 '40K_ROCK_2_40K_NA': [3.72E+04, 1000], \
-'234Pa_PMT_CHAIN_238U_NA': [5.13E+03 *iPMTs * uip, 1], \
-'214Pb_PMT_CHAIN_238U_NA': [5.13E+03 *iPMTs * uip, 1], \
-'214Bi_PMT_CHAIN_238U_NA': [5.13E+03 *iPMTs * uip, 1], \
-'210Bi_PMT_CHAIN_238U_NA': [5.13E+03 *iPMTs * uip, 1], \
-'210Tl_PMT_CHAIN_238U_NA': [5.13E+03*0.002 *iPMTs * uip, 1], \
+'234Pa_PMT_CHAIN_238U_NA': [5.13E+03 * uip, 1], \
+'214Pb_PMT_CHAIN_238U_NA': [5.13E+03 * uip, 1], \
+'214Bi_PMT_CHAIN_238U_NA': [5.13E+03 * uip, 1], \
+'210Bi_PMT_CHAIN_238U_NA': [5.13E+03 * uip, 1], \
+'210Tl_PMT_CHAIN_238U_NA': [5.13E+03*0.002 * uip, 1], \
 '234Pa_VETO_CHAIN_238U_NA': [0, 1], \
 '214Pb_VETO_CHAIN_238U_NA': [0, 1], \
 '214Bi_VETO_CHAIN_238U_NA': [0, 1], \
@@ -530,20 +535,20 @@ def loadSimulationParameters():
 '214Bi_ROCK_2_CHAIN_238U_NA': [7.71E+06, 1], \
 '210Bi_ROCK_2_CHAIN_238U_NA': [7.71E+06, 1], \
 '210Tl_ROCK_2_CHAIN_238U_NA': [7.71E+06*0.0002, 1], \
-'234Pa_GD_CHAIN_238U_NA': [8.28e-01 *pmtVolCorr, 1], \
-'214Pb_GD_CHAIN_238U_NA': [8.28e-01 *pmtVolCorr, 1], \
-'214Bi_GD_CHAIN_238U_NA': [8.28e-01 *pmtVolCorr, 1], \
-'210Bi_GD_CHAIN_238U_NA': [8.28e-01 *pmtVolCorr, 1], \
-'210Tl_GD_CHAIN_238U_NA': [8.28e-01*0.0002 *pmtVolCorr, 1], \
-'234Pa_LIQUID_CHAIN_238U_NA': [8.35*pmtVolCorr , 1], \
-'214Pb_LIQUID_CHAIN_238U_NA': [8.35*pmtVolCorr , 1], \
-'214Bi_LIQUID_CHAIN_238U_NA': [8.35*pmtVolCorr , 1], \
-'210Bi_LIQUID_CHAIN_238U_NA': [8.35*pmtVolCorr , 1], \
-'210Tl_LIQUID_CHAIN_238U_NA': [8.35*0.0002*pmtVolCorr , 1], \
-'228Ac_PMT_CHAIN_232Th_NA': [4.49E+03 *iPMTs * tip, 1], \
-'212Pb_PMT_CHAIN_232Th_NA': [4.49E+03 *iPMTs * tip, 1], \
-'212Bi_PMT_CHAIN_232Th_NA': [4.49E+03*0.64 *iPMTs * tip, 1], \
-'208Tl_PMT_CHAIN_232Th_NA': [4.49E+03*0.36 *iPMTs * tip, 1], \
+'234Pa_GD_CHAIN_238U_NA': [8.28e-01 , 1], \
+'214Pb_GD_CHAIN_238U_NA': [8.28e-01 , 1], \
+'214Bi_GD_CHAIN_238U_NA': [8.28e-01 , 1], \
+'210Bi_GD_CHAIN_238U_NA': [8.28e-01 , 1], \
+'210Tl_GD_CHAIN_238U_NA': [8.28e-01*0.0002 , 1], \
+'234Pa_LIQUID_CHAIN_238U_NA': [8.35 , 1], \
+'214Pb_LIQUID_CHAIN_238U_NA': [8.35 , 1], \
+'214Bi_LIQUID_CHAIN_238U_NA': [8.35 , 1], \
+'210Bi_LIQUID_CHAIN_238U_NA': [8.35 , 1], \
+'210Tl_LIQUID_CHAIN_238U_NA': [8.35*0.0002 , 1], \
+'228Ac_PMT_CHAIN_232Th_NA': [4.49E+03 * tip, 1], \
+'212Pb_PMT_CHAIN_232Th_NA': [4.49E+03 * tip, 1], \
+'212Bi_PMT_CHAIN_232Th_NA': [4.49E+03*0.64 * tip, 1], \
+'208Tl_PMT_CHAIN_232Th_NA': [4.49E+03*0.36 * tip, 1], \
 '228Ac_VETO_CHAIN_232Th_NA': [0, 1], \
 '212Pb_VETO_CHAIN_232Th_NA': [0, 1], \
 '212Bi_VETO_CHAIN_232Th_NA': [0*0.64, 1], \
@@ -564,14 +569,14 @@ def loadSimulationParameters():
 '212Pb_ROCK_2_CHAIN_232Th_NA': [1.19E+06, 1000], \
 '212Bi_ROCK_2_CHAIN_232Th_NA': [1.19E+06*0.64, 1000], \
 '208Tl_ROCK_2_CHAIN_232Th_NA': [1.19E+06*0.36, 1000], \
-'228Ac_GD_CHAIN_232Th_NA': [4.14E-01*pmtVolCorr , 1], \
-'212Pb_GD_CHAIN_232Th_NA': [4.14E-01*pmtVolCorr , 1], \
-'212Bi_GD_CHAIN_232Th_NA': [4.14E-01*0.64*pmtVolCorr , 1], \
-'208Tl_GD_CHAIN_232Th_NA': [4.14E-01*0.36*pmtVolCorr , 1], \
-'228Ac_LIQUID_CHAIN_232Th_NA': [8.35E-01*pmtVolCorr , 1], \
-'212Pb_LIQUID_CHAIN_232Th_NA': [8.35E-01*pmtVolCorr , 1], \
-'212Bi_LIQUID_CHAIN_232Th_NA': [8.35E-01*0.64*pmtVolCorr , 1], \
-'208Tl_LIQUID_CHAIN_232Th_NA': [8.35E-01*0.36*pmtVolCorr , 1], \
+'228Ac_GD_CHAIN_232Th_NA': [4.14E-01 , 1], \
+'212Pb_GD_CHAIN_232Th_NA': [4.14E-01 , 1], \
+'212Bi_GD_CHAIN_232Th_NA': [4.14E-01*0.64 , 1], \
+'208Tl_GD_CHAIN_232Th_NA': [4.14E-01*0.36 , 1], \
+'228Ac_LIQUID_CHAIN_232Th_NA': [8.35E-01 , 1], \
+'212Pb_LIQUID_CHAIN_232Th_NA': [8.35E-01 , 1], \
+'212Bi_LIQUID_CHAIN_232Th_NA': [8.35E-01*0.64 , 1], \
+'208Tl_LIQUID_CHAIN_232Th_NA': [8.35E-01*0.36 , 1], \
 '231Th_IBEAM_CHAIN_235U_NA': [3.84e+03, 50], \
 '223Fr_IBEAM_CHAIN_235U_NA': [3.84e+03*0.0138, 50], \
 '211Pb_IBEAM_CHAIN_235U_NA': [3.84e+03, 50], \
@@ -587,19 +592,19 @@ def loadSimulationParameters():
 '211Pb_TANK_CHAIN_235U_NA': [6.09E+03, 50], \
 '211Bi_TANK_CHAIN_235U_NA': [6.09E+03*0.00270, 50], \
 '207Tl_TANK_CHAIN_235U_NA': [6.09E+03, 50], \
-'231Th_LIQUID_CHAIN_235U_NA': [3.89E-01*pmtVolCorr , 1], \
-'223Fr_LIQUID_CHAIN_235U_NA': [3.89E-01*0.0138*pmtVolCorr, 1], \
-'211Pb_LIQUID_CHAIN_235U_NA': [3.89E-01*pmtVolCorr , 1], \
-'211Bi_LIQUID_CHAIN_235U_NA': [3.89E-01*0.00270*pmtVolCorr , 1], \
-'207Tl_LIQUID_CHAIN_235U_NA': [3.89E-01*pmtVolCorr , 1], \
+'231Th_LIQUID_CHAIN_235U_NA': [3.89E-01 , 1], \
+'223Fr_LIQUID_CHAIN_235U_NA': [3.89E-01*0.0138, 1], \
+'211Pb_LIQUID_CHAIN_235U_NA': [3.89E-01 , 1], \
+'211Bi_LIQUID_CHAIN_235U_NA': [3.89E-01*0.00270 , 1], \
+'207Tl_LIQUID_CHAIN_235U_NA': [3.89E-01 , 1], \
 '60Co_IBEAM_60Co_NA': [4.30e+04, 50], \
 '137Cs_IBEAM_137Cs_NA': [4.53e+04, 50], \
 '60Co_TANK_60Co_NA': [6.81E+04, 50], \
 '137Cs_TANK_137Cs_NA': [7.18E+04, 50], \
 '60Co_PSUP_60Co_NA': [5.93E+03, 50], \
 '137Cs_PSUP_137Cs_NA': [6.24E+03, 50], \
-'li9_LIQUID_A_Z': [1.056e-05*pmtVolCorr , 1], \
-'n17_LIQUID_A_Z': [1.061e-5*pmtVolCorr , 1],\
+'li9_LIQUID_A_Z': [1.056e-05 , 1], \
+'n17_LIQUID_A_Z': [1.061e-5 , 1],\
 'singles_ALL_singles': [1,1000],\
 'mono_LIQUID_e-':[1,1],\
 'mono_LIQUID_e+':[1,1],\
